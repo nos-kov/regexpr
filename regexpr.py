98a661486d284487816fac29c1dab837cf194a63
Dima <dvnoskov88@gmail.com>
2023-08-14 15:24:10 +0300

    done

--- regexpr.py
+++ regexpr.py
@@ -1,9 +1,23 @@
 from pprint import pprint
+import csv
 import re
 
 keywords = ('Минфин', 'ФНС')
 
+def regexp(clean_list):
+    pattern = r"(\+7|8)\s*\(*(\d\d\d)\)*(\D*|\-)(\d\d\d)(\D*)(\d\d)(\D*)(\d\d)(\s*)(\(*)(доб.\s\d\d\d\d)*\)*"
+    replace = r"+7(\2)\4-\6-\8 \11"
+    for element in clean_list:
+        element[5] = re.sub(pattern, replace, element[5])
+        print(element[5])
+
+
+    return clean_list
+
+
 def remove_dups(mylist):
+    '''removes the duplicates and condenses the list'''
+
     marker_set = set()
     dup_free = []
     flatten = []
@@ -45,68 +59,69 @@ def remove_dups(mylist):
             
     return dup_free
 
-import csv
-with open("phonebook_raw.csv", encoding="utf8") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-
-semiresult = []
-
-for element in contacts_list:
-    clean1 = element[0].split()
-    clean2 = element[1].split()
-    new_element =[]
-    new_element = clean1 + clean2 + new_element
-    for id_el, nested_element in enumerate(element):
-
-        if id_el > 1:
-            
-            nested_element_list = [nested_element]
-            new_element = new_element + nested_element_list
-
-    semiresult.append(new_element)
-
-clean_list = remove_dups(semiresult)
+if __name__ == '__main__':
 
 
-for idx, data in enumerate(clean_list):
-    if idx > 0: 
-        fixed = False
-        flag, flag1, flag2, flag3 = (False,)*4
-        for idy, result_element in enumerate(data):
-            if fixed == True: continue
-            if idy > 2: 
+    with open("phonebook_raw.csv", encoding="utf8") as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+
+    semiresult = []
+
+    for element in contacts_list:
+        clean1 = element[0].split()
+        clean2 = element[1].split()
+        new_element =[]
+        new_element = clean1 + clean2 + new_element
+        for id_el, nested_element in enumerate(element):
+
+            if id_el > 1:
                 
-                if result_element in keywords and idy != 3: 
-                    data[3] = result_element
-                    data[idy] = ''
-                    flag = True
-                            
-                elif '@' in result_element:
-                    data[6] = result_element
-                    data.pop(idy)
-                    flag1 = True
+                nested_element_list = [nested_element]
+                new_element = new_element + nested_element_list
 
-                elif re.search(r'\d\d\d', result_element) and '@' not in result_element:
-                    data[5] = result_element
-                    data[idy] = ''
-                    flag2 = True
-                elif result_element == '':
-                    pass
-                else:
-                    if result_element in keywords: continue
-                    data[4] = result_element
-                    if idy != 4:
+        semiresult.append(new_element)
+
+    clean_list = remove_dups(semiresult)
+
+
+    for idx, data in enumerate(clean_list):
+        if idx > 0: 
+            fixed = False
+            flag, flag1, flag2, flag3 = (False,)*4
+            for idy, result_element in enumerate(data):
+                if fixed == True: continue
+                if idy > 2: 
+                    
+                    if result_element in keywords and idy != 3: 
+                        data[3] = result_element
                         data[idy] = ''
-                    flag3 = True
-                if all((flag,flag1,flag2,flag3)) : fixed = True
+                        flag = True
+                                
+                    elif '@' in result_element:
+                        data[6] = result_element
+                        data.pop(idy)
+                        flag1 = True
 
-pprint(clean_list)
+                    elif re.search(r'\d\d\d', result_element) and '@' not in result_element:
+                        data[5] = result_element
+                        data[idy] = ''
+                        flag2 = True
+                    elif result_element == '':
+                        pass
+                    else:
+                        if result_element in keywords: continue
+                        data[4] = result_element
+                        if idy != 4:
+                            data[idy] = ''
+                        flag3 = True
+                    if all((flag,flag1,flag2,flag3)) : fixed = True
+
+regexp(clean_list)
 
 with open("phonebook.csv", "w") as f:
     datawriter = csv.writer(f, delimiter=',')
-  
     datawriter.writerows(clean_list)
 
-#(\+7|8)\s*\(*(\d\d\d)(\)*|(\d*)|\-*)\s*(\d\d\d)(\d|\-)(\d\d)((\d)|\-)(\d*)\s*\(*(доб.)*\s*(\d*)\)*
-#+7(\2)\5-\7
+#(\+7|8)\s*\(*(\d\d\d)\)*(\D*|\-)(\d\d\d)(\D*)(\d\d)(\D*)(\d\d)(\s*)(\(*)(доб.\s\d\d\d\d)*\)*
+#+7(\2)\4-\6-\8 \11
